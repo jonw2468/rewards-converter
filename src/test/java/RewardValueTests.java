b706bc840c79cc9210189ce6e3d0c41f60f3a491
jonw2468 <jwoods6@umbc.org>
2023-08-25 11:58:17 -0400

    Merge remote-tracking branch 'origin/reward-value' into reward-value
    # Conflicts:
    #	src/main/java/RewardValue.java

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -5,26 +5,32 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class RewardValueTests {
 
     @Test
-    void create_with_cash_value() {
+    public void create_with_cash_value() {
         double cashValue = 100;
         var rewardValue = new RewardValue(cashValue);
-        assertEquals(cashValue, rewardValue.getCashValue());
+        assertEquals(cashValue, rewardValue.getCashValue(), 0.0001);
     }
 
     @Test
-    void create_with_miles_value() {
+    public void create_with_miles_value() {
         int milesValue = 10000;
         var rewardValue = new RewardValue(milesValue);
         assertEquals(milesValue, rewardValue.getMilesValue());
     }
 
     @Test
-    void convert_from_cash_to_miles() {
-        assert false;
+    public void convert_from_cash_to_miles() {
+        double cashValue = 100;
+        int excpectedMiles = (int)(cashValue / RewardValue.MILES_TO_CASH);
+        var rewardValue = new RewardValue(cashValue);
+        assertEquals(excpectedMiles, rewardValue.getMilesValue());
     }
 
     @Test
-    void convert_from_miles_to_cash() {
-        assert false;
+    public void convert_from_miles_to_cash() {
+        int milesValue = 10000;
+        var rewardValue = new RewardValue(milesValue);
+        double expectedCash = (double)milesValue * RewardValue.MILES_TO_CASH;
+        assertEquals(expectedCash, rewardValue.getCashValue(), 0.0001);
     }
 }
